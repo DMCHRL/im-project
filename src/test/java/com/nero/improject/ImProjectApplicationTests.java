623a0c5c2d84da240f57b6a7e1961da5524022fd
Nero <1529730472@qq.com>
2020-12-25 17:15:22 +0800

    init

---
+++ src/test/java/com/nero/improject/ImProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nero.improject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
